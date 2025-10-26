b20102f716e3bec67a3adccc4b37023938032a19
Łukasz Sadowski <93434596+Sadi2002@users.noreply.github.com>
2025-10-26 12:51:10 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,22 +5,7 @@ import HeaderMobile from "./components/header/mobile/HeaderMobile";
 import SecondSection from "./components/secondSection/mobile/SecondSection";
 import ThirdSection from "./components/thirdSection/mobile/ThirdSection";
 
-import Message from "./components/Message";
-import { useEffect, useState } from "react";
-
 export default function Home() {
-  const [isMobile, setIsMobile] = useState(true); // zakładamy, że mobile
-
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 768);
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  if (!isMobile) return <Message />;
 
   return (
     <>
